6345cbe92150ab65d9c460e5b6522d7fcee6cb46
user-nik <nik.pokryshkin@gmail.com>
2021-01-17 17:18:35 +0300

    addded update/delete/insertBulk functions

--- ProductGrpc/Services/ProductService.cs
+++ ProductGrpc/Services/ProductService.cs
@@ -34,11 +34,13 @@ namespace ProductGrpc.Services
         public override async Task<ProductModel> GetProduct(GetProductRequest request,
             ServerCallContext context)
         {
-            var product = await _productContext.Products.FindAsync(request.ProductId);
+            var product = await _productContext.Products
+                .AsNoTracking()
+                .FirstOrDefaultAsync(r=>r.ProductId == request.ProductId);
             if (product == null)
             {
                 throw new RpcException(new Status(StatusCode.NotFound, "There is no product"));
-            }
+            } 
 
             var productModel = _mapper.Map<ProductModel>(product);
 
@@ -78,6 +80,80 @@ namespace ProductGrpc.Services
 
             var productModel = _mapper.Map<ProductModel>(product);
             return productModel;
+        }
+
+        public override async Task<ProductModel> UpdateProduct(UpdateProductRequest request,
+            ServerCallContext context)
+        {
+            var product = _mapper.Map<Product>(request.Product);
+
+            bool isExists = await _productContext.Products
+                .AnyAsync(p=> p.ProductId == product.ProductId);
+
+            if (!isExists)
+            {
+               throw new RpcException(new Status(StatusCode.NotFound, "There is no product"));
+            }
+
+            _productContext.Entry(product).State = EntityState.Modified;
+            try
+            {
+                await _productContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw;
+            }
+
+            var productModel = _mapper.Map<ProductModel>(product);
+
+            return productModel;
+        }
+
+        public override async Task<DeleteProductModelResponse> DeleteProduct(DeleteProductRequest request,
+            ServerCallContext context)
+        {
+            var product = await _productContext.Products.FindAsync(request.ProductId);
+            if (product == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "There is no product"));
+            }
+
+            _productContext.Products.Remove(product);
+            var deleteCount = await _productContext.SaveChangesAsync();
+
+            var reponse = new DeleteProductModelResponse
+            {
+                Success = deleteCount > 0
+            };
+
+            return reponse;
+        }
+
+        public override async Task<InsertBulkProductReponse> InsertBulkProduct(IAsyncStreamReader<ProductModel> requestStream,
+            ServerCallContext context)
+        {
+            await foreach (var item in requestStream.ReadAllAsync())
+            {
+                var product = _mapper.Map<Product>(item);
+                _productContext.Products.Add(product);
+            }
+
+            //while (await requestStream.MoveNext())
+            //{
+            //    var product = _mapper.Map<Product>(requestStream.Current);
+            //    _productContext.Products.Add(product);
+            //}
+
+            var insertCount = await _productContext.SaveChangesAsync();
+
+            var response = new InsertBulkProductReponse
+            {
+                InsertCount = insertCount,
+                Success = insertCount > 0,
+            };
+
+            return response;
         }
     }
 }
--- ProductGrpcClient/Program.cs
+++ ProductGrpcClient/Program.cs
@@ -20,11 +20,24 @@ namespace ProductGrpcClient
                 await GetProductAsync(client);
                 await Task.Delay(500);
 
-                await AddProductAsync(client);
+                //await AddProductAsync(client);
+                await Task.Delay(500);
+
+                await UpdateProductsAsync(client);
+                await Task.Delay(500);
+
+                //await DeleteProductsAsync(client);
                 await Task.Delay(500);
 
                 await GetAllProductsAsync(client);
                 await Task.Delay(500);
+
+                await InsertBulkProduct(client);
+                await Task.Delay(500);
+
+                await GetAllProductsAsync(client);
+                await Task.Delay(500);
+
             }
             catch (RpcException ex)
             {
@@ -35,6 +48,35 @@ namespace ProductGrpcClient
             Console.ReadKey();
         }
 
+        private static async Task InsertBulkProduct(ProductsProtoService.ProductsProtoServiceClient client)
+        {
+            Console.WriteLine("InsertBulkProduct started..");
+
+            using var clientBulk = client.InsertBulkProduct();
+
+            for (int i = 0; i < 3; i++)
+            {
+                var productModel = new ProductModel
+                {
+                    Created = Timestamp.FromDateTime(DateTime.UtcNow),
+                    Description = "Bulk Added Product",
+                    Name = $"Name {i}",
+                    Price = 100 * i,
+                    Status = ProductStatus.Instock
+                };
+
+                await clientBulk.RequestStream.WriteAsync(productModel);
+
+            }
+
+            await clientBulk.RequestStream.CompleteAsync();
+
+            var responseBulk = await clientBulk;
+
+            Console.WriteLine("InsertBulkProduct ended..");
+            Console.WriteLine($"Status {responseBulk.Success} Inserted: {responseBulk.InsertCount}");
+
+        }
 
         private static async Task GetAllProductsAsync(ProductsProtoService.ProductsProtoServiceClient client)
         {
@@ -79,6 +121,42 @@ namespace ProductGrpcClient
                 );
 
             Console.WriteLine("GetAllProducts started.." + addProductResponse.ToString());
+
+        }
+
+
+        private static async Task DeleteProductsAsync(ProductsProtoService.ProductsProtoServiceClient client)
+        {
+            Console.WriteLine("DeleteProductsAsync started..");
+
+            var deleteReponse = await client.DeleteProductAsync(
+               new DeleteProductRequest
+               {
+                   ProductId = 2,
+               });
+           
+            Console.WriteLine("DeleteProductsAsync response.. " + deleteReponse.ToString());
+
+        }
+
+        private static async Task UpdateProductsAsync(ProductsProtoService.ProductsProtoServiceClient client)
+        {
+            Console.WriteLine("UpdateProductsAsync started..");
+            var updateReponse = await client.UpdateProductAsync(
+                new UpdateProductRequest
+                {
+                    Product = new ProductModel
+                    {
+                        Created = Timestamp.FromDateTime(DateTime.UtcNow),
+                        Description = "Updated Product",
+                        Name = "Name",
+                        Price = 1799,
+                        Status = ProductStatus.Instock,
+                        ProductId = 1
+                    }
+                });
+
+            Console.WriteLine("UpdateProductsAsync response.. " + updateReponse.ToString());
 
         }
     }
